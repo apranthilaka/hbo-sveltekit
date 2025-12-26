003c6c56dd982e3545035ad037be3235ee35a03b
Aravinda <apranthilaka@outlook.com>
2025-12-27 00:27:37 +0530

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        // adapter-static is required for GitHub Pages
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
-            fallback: undefined,
+            fallback: '404.html', // Allows for SPA-like routing on GitHub
             precompress: false,
             strict: true,
         }),
+        paths: {
+            // Replace 'your-repo-name' with your actual repository name
+            // If you are using a custom domain (e.g. www.example.com), set this to ''
+            base: process.argv.includes('dev') ? '' : '/hbo-sveltekit',
+        },
     },
 };
 
