bf9f97fc8d8f14139aca7469e6eb5b9c358ee6d3
Aravinda <apranthilaka@outlook.com>
2025-12-27 00:37:43 +0530

    test 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,4 @@
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
     documentation
 </p>
-<a href="{base}/about">About Page</a>
+<a href="{base}/about">About Page Goes here</a>
