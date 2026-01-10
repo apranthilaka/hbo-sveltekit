ce3f77f640b45b18df9f069b7d7b34cac2ed36d4
Aravinda <apranthilaka@outlook.com>
2026-01-10 18:51:01 +0530

    db fix 3

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
+import {
+    PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY,
+} from '$env/static/public';
 
-// This allows the app to check for the keys at runtime
 export const supabase = createClient(
-    env.PUBLIC_SUPABASE_URL,
-    env.PUBLIC_SUPABASE_ANON_KEY
+    PUBLIC_SUPABASE_URL,
+    PUBLIC_SUPABASE_ANON_KEY
 );
