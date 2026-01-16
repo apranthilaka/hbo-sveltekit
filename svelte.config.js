69317007027460c6fd6fc0093edbff14a35ea060
Aravinda <apranthilaka@outlook.com>
2026-01-16 10:58:41 +0530

    config update 3

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        // adapter-static is required for GitHub Pages
         adapter: adapter({
-            // default output directory is 'build'
             pages: 'build',
             assets: 'build',
-            fallback: '404.html', // Recommended for SPA behavior on GitHub Pages
-            precompress: false,
-            strict: true,
+            fallback: '404.html',
         }),
-
         paths: {
-            // Set this to your repository name
-            base: process.env.NODE_ENV === 'production' ? '/hbo-sveltekit' : '',
+            // Only use the repo name in production
+            base: dev ? '' : '/hbo-sveltekit',
         },
     },
 };
